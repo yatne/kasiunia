c882506050953d13d51477fcab7739a741c06514
Antoni Smoliński <a.smolinski@visuality.pl>
2021-06-25 18:02:07 +0200

    zmiany zmiany

--- src/components/Layout/Layaout.js
+++ src/components/Layout/Layaout.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export default styled.div`
+  dipslay: flex;
+  flex-direction: column;
+  align-items: center;
   max-width: 2000px;
   margin: auto;
   background-color: #eeeeee
